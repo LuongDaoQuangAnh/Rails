30a40fa6fb030c69d0212313465fbab20022a734
Rails <aluong343@gmail.com>
2020-04-19 01:41:24 +0700

    Make app product

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'pages/home', to: 'pages#home'
   #
-  root 'pages#home'
+  root 'welcome#home'
 
-  get 'about', to: 'pages#about'
+  get 'about', to: 'welcome#about'
   # get 'pages/about', to: 'pages#about'
 end
