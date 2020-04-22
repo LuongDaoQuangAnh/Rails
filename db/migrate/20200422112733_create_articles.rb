c509a75bda634c3ee457e24065dcbbfd387108b6
Rails <aluong343@gmail.com>
2020-04-22 20:41:08 +0700

    secure password

--- db/migrate/20200422112733_create_articles.rb
+++ db/migrate/20200422112733_create_articles.rb
@@ -1,7 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string "title"
+      t.string :title
+      t.integer :user_id
+      t.text :description
+      t.timestamps
     end
   end
 end
